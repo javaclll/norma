ed21136edeccf561c2646ae7932820939b0895d1
Daze <dazehere@yandex.com>
2021-12-07 17:27:29 +0545

    [feat] Coordinate to PGN

--- web/src/components/Board/Board.jsx
+++ web/src/components/Board/Board.jsx
@@ -14,132 +14,133 @@ import tiger from "../../statics/tiger.svg";
 import goat from "../../statics/goat.svg";
 
 const Board = () => {
-	const {
-		game,
-		setTurn,
-		setGame,
-		setGoatCounter,
-		setMoveCounter,
-		setGameResult,
-		setMoveHistory,
-		setGoatsCaptured,
-		startingLayout,
-		gameResult,
-		turn,
-		goatsCaptured,
-		goatCounter,
-		nextMove,
-		previousMove,
-		moveCounter,
-		moveHistory,
-	} = useContext(GameContext);
+  const {
+    game,
+    setTurn,
+    setGame,
+    setGoatCounter,
+    setMoveCounter,
+    setGameResult,
+    setMoveHistory,
+    setGoatsCaptured,
+    startingLayout,
+    gameResult,
+    turn,
+    goatsCaptured,
+    goatCounter,
+    nextMove,
+    previousMove,
+    moveCounter,
+    moveHistory,
+    pgn
+  } = useContext(GameContext);
 
-	const onClickHandler = () => {
-		setGameResult({ decided: false });
-		setGoatsCaptured(0);
-		setTurn(ItemTypes.GOAT);
-		setMoveHistory([startingLayout]);
-		setGame(startingLayout);
-		setMoveCounter(0);
-		setGoatCounter(0);
-	};
-	return (
-		<>
-			<DndProvider backend={HTML5Backend}>
-				<div className="board" style={{ backgroundImage: `url(${board})` }}>
-					<div className="left-box">
-						<div className="left-box-content">
-							<div className="left-box-content-top">
-								<a className="left-box-title">Played moves</a>
-							</div>
-							<div className="left-box-content-bottom">{moveHistory}</div>
-						</div>
-					</div>
-					<div className="tableDiv">
-						<table className="boardTable">
-							<tbody>
-								{game.map((row, rowIndex) => (
-									<tr className="row" key={rowIndex}>
-										{row.map((value, colIndex) => (
-											<Intersection
-												x={rowIndex}
-												y={colIndex}
-												key={`(${rowIndex},${colIndex})`}
-											>
-												<CoRe condition={value === 1}>
-													<Goat m={rowIndex} n={colIndex} />
-												</CoRe>
-												<CoRe condition={value === -1}>
-													<Tiger m={rowIndex} n={colIndex} />
-												</CoRe>
-											</Intersection>
-										))}
-									</tr>
-								))}
-							</tbody>
-						</table>
-					</div>
-				</div>
-			</DndProvider>
+  const onClickHandler = () => {
+    setGameResult({ decided: false });
+    setGoatsCaptured(0);
+    setTurn(ItemTypes.GOAT);
+    setMoveHistory([startingLayout]);
+    setGame(startingLayout);
+    setMoveCounter(0);
+    setGoatCounter(0);
+  };
+  return (
+    <>
+      <DndProvider backend={HTML5Backend}>
+        <div className="board" style={{ backgroundImage: `url(${board})` }}>
+          <div className="left-box">
+            <div className="left-box-content">
+              <div className="left-box-content-top">
+                <a className="left-box-title">Played moves</a>
+              </div>
+              <div className="left-box-content-bottom">{pgn.join(" - ")}</div>
+            </div>
+          </div>
+          <div className="tableDiv">
+            <table className="boardTable">
+              <tbody>
+                {game.map((row, rowIndex) => (
+                  <tr className="row" key={rowIndex}>
+                    {row.map((value, colIndex) => (
+                      <Intersection
+                        x={rowIndex}
+                        y={colIndex}
+                        key={`(${rowIndex},${colIndex})`}
+                      >
+                        <CoRe condition={value === 1}>
+                          <Goat m={rowIndex} n={colIndex} />
+                        </CoRe>
+                        <CoRe condition={value === -1}>
+                          <Tiger m={rowIndex} n={colIndex} />
+                        </CoRe>
+                      </Intersection>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </DndProvider>
 
-			<div className="right-box">
-				<div className="button-panel">
-					<button
-						className="backward button"
-						onClick={previousMove}
-						disabled={moveCounter === 0}
-					>
-						<img className="arrow" src={arrow} />
-					</button>
-					<div className="move-number">{moveCounter}</div>
-					<button
-						className="forward button"
-						onClick={nextMove}
-						disabled={moveCounter === moveHistory.length - 1}
-					>
-						<img className="arrow" src={arrow} />
-					</button>
-				</div>
+      <div className="right-box">
+        <div className="button-panel">
+          <button
+            className="backward button"
+            onClick={previousMove}
+            disabled={moveCounter === 0}
+          >
+            <img className="arrow" src={arrow} />
+          </button>
+          <div className="move-number">{moveCounter}</div>
+          <button
+            className="forward button"
+            onClick={nextMove}
+            disabled={moveCounter === moveHistory.length - 1}
+          >
+            <img className="arrow" src={arrow} />
+          </button>
+        </div>
 
-				<div className="detail-box">
-					<CoRe condition={!gameResult.decided}>
-						<h2>{turn === ItemTypes.GOAT ? "Goat" : "Tiger"}'s Turn</h2>
-					</CoRe>
-					<CoRe condition={gameResult.decided}>
-						<h2>{gameResult.wonBy === -1 ? "Tiger" : "Goat"} Won!</h2>
-					</CoRe>
-					<h2>Captured Goats: {goatsCaptured}</h2>
-					<h2>Placed Goats: {goatCounter}</h2>
-					<h2>History Length: {moveHistory.length}</h2>
-				</div>
-			</div>
+        <div className="detail-box">
+          <CoRe condition={!gameResult.decided}>
+            <h2>{turn === ItemTypes.GOAT ? "Goat" : "Tiger"}'s Turn</h2>
+          </CoRe>
+          <CoRe condition={gameResult.decided}>
+            <h2>{gameResult.wonBy === -1 ? "Tiger" : "Goat"} Won!</h2>
+          </CoRe>
+          <h2>Captured Goats: {goatsCaptured}</h2>
+          <h2>Placed Goats: {goatCounter}</h2>
+          <h2>History Length: {moveHistory.length}</h2>
+        </div>
+      </div>
 
-			{gameResult.decided ? (
-				<>
-					<div className="win-screen">
-						<div className="win-screen-top">
-							<img
-								src={turn === ItemTypes.GOAT ? tiger : goat}
-								className="win-screen-img"
-							/>
-							<a className="win-screen-text">
-								{turn === ItemTypes.GOAT ? "Tiger" : "Goat"} Wins!
-							</a>
-						</div>
-						<div className="win-screen-bottom">
-							<p
-								className="win-screen-play-again-button"
-								onClick={onClickHandler}
-							>
-								Play again!
-							</p>
-						</div>
-					</div>
-				</>
-			) : (
-				<></>
-			)}
-		</>
-	);
+      {gameResult.decided
+        ? (
+          <>
+            <div className="win-screen">
+              <div className="win-screen-top">
+                <img
+                  src={turn === ItemTypes.GOAT ? tiger : goat}
+                  className="win-screen-img"
+                />
+                <a className="win-screen-text">
+                  {turn === ItemTypes.GOAT ? "Tiger" : "Goat"} Wins!
+                </a>
+              </div>
+              <div className="win-screen-bottom">
+                <p
+                  className="win-screen-play-again-button"
+                  onClick={onClickHandler}
+                >
+                  Play again!
+                </p>
+              </div>
+            </div>
+          </>
+        )
+        : <></>}
+    </>
+  );
 };
 export default Board;
--- web/src/context/GameContext.jsx
+++ web/src/context/GameContext.jsx
@@ -11,7 +11,6 @@ export const startingLayout = [
   [-1, 0, 0, 0, -1],
 ];
 
-
 // export const startingLayout = [
 //   [1, 1, 1, 1, 1],
 //   [1, 1, 1, 1, 1],
@@ -28,6 +27,8 @@ export const GameProvider = ({ children }) => {
   const [moveCounter, setMoveCounter] = useState(0);
 
   const [goatsCaptured, setGoatsCaptured] = useState(0);
+
+  const [pgn, setPGN] = useState([]);
 
   const [gameResult, setGameResult] = useState({ decided: false });
 
@@ -62,7 +63,7 @@ export const GameProvider = ({ children }) => {
                     target: [i + k, j + l],
                   },
                   position,
-                  ItemTypes.TIGER
+                  ItemTypes.TIGER,
                 );
                 if (thisPieceHasAMove["isValid"]) {
                   return true;
@@ -80,6 +81,30 @@ export const GameProvider = ({ children }) => {
     }
 
     return { decided: false };
+  };
+
+  const cordToPGN = ({ source: [x, y], target: [m, n] }) => {
+    let secondChar = x === "X" ? x : (4 - x).toString();
+    let fourthChar = m === "X" ? m : (4 - m).toString();
+    // let fourthChar = m.toString();
+
+    let [firstChar, thirdChar] = [y, n].map((item) => {
+      switch (item) {
+        case 0:
+          return "A";
+        case 1:
+          return "B";
+        case 2:
+          return "C";
+        case 3:
+          return "D";
+        case 4:
+          return "E";
+        default:
+          return "X";
+      }
+    });
+    return firstChar + secondChar + thirdChar + fourthChar;
   };
 
   const makeMove = ({ source: [x, y], target: [m, n] }) => {
@@ -107,7 +132,7 @@ export const GameProvider = ({ children }) => {
 
       let gameStatusAfterMove = gameStatusCheck(
         new_state,
-        eval_move["isCaptureMove"]
+        eval_move["isCaptureMove"],
       );
       if (gameStatusAfterMove.decided === true) {
         setGameResult(gameStatusAfterMove);
@@ -115,6 +140,7 @@ export const GameProvider = ({ children }) => {
 
       setGame(new_state);
       setMoveCounter(moveCounter + 1);
+      setPGN([...pgn, cordToPGN({ source: [x, y], target: [m, n] })]);
     }
   };
 
@@ -141,26 +167,27 @@ export const GameProvider = ({ children }) => {
       setGame(new_state);
       setGoatCounter(goatCounter + 1);
       setTurn(ItemTypes.TIGER);
+      setPGN([...pgn, cordToPGN({ source: ["X", "X"], target: [m, n] })]);
     }
   };
 
   const checkMove = (
     { source: [x, y], target: [m, n] },
     position = game,
-    assumingTurn = turn
+    assumingTurn = turn,
   ) => {
     if (x < 0 || y < 0 || m < 0 || n < 0 || x > 4 || y > 4 || m > 4 || n > 4) {
-      console.log(`Reason 1`)
+      console.log(`Reason 1`);
       return { isValid: false };
     }
 
     if (gameResult.decided) {
-      console.log(`Reason 2`)
+      console.log(`Reason 2`);
       return { isValid: false };
     }
 
     if (moveCounter + 1 !== moveHistory.length) {
-      console.log(`Reason 3, Move count: ${moveHistory.length} ${moveCounter}`)
+      console.log(`Reason 3, Move count: ${moveHistory.length} ${moveCounter}`);
       return { isValid: false };
     }
 
@@ -171,21 +198,21 @@ export const GameProvider = ({ children }) => {
         (assumingTurn === ItemTypes.TIGER && position[x][y] === -1)
       )
     ) {
-      console.log(`Reason 4`)
+      console.log(`Reason 4`);
       return { isValid: false };
     }
 
     // Can't move goat before all goats are placed
     if (assumingTurn === ItemTypes.GOAT) {
       if (goatCounter < 20) {
-        console.log(`Reason 5`)
+        console.log(`Reason 5`);
         return { isValid: false };
       }
     }
 
     // If target has some piece
     if (position[m][n] !== 0) {
-      console.log(`Reason 6`)
+      console.log(`Reason 6`);
       return { isValid: false };
     }
 
@@ -198,80 +225,79 @@ export const GameProvider = ({ children }) => {
 
     // Source and target can't be same
     if (xDiffAbs === 0 && yDiffAbs === 0) {
-      console.log(`Reason 7`)
+      console.log(`Reason 7`);
       return { isValid: false };
     }
 
     // Tiger can jump goats
-    if (   // (2,0), (2,2), (0, 2), (2, 2)
+    if (
+      // (2,0), (2,2), (0, 2), (2, 2)
       assumingTurn === ItemTypes.TIGER &&
-      ((xDiffAbs === 2 && yDiffAbs === 0 ) ||
+      ((xDiffAbs === 2 && yDiffAbs === 0) ||
         (yDiffAbs === 2 && (xDiffAbs === 0 || xDiffAbs === 2)))
     ) {
-      if(xDiffAbs === 2 && yDiffAbs === 2){
-        if (sSum%2 !== 0){
-          console.log(`Reason 13`)
-          return { isValid: false }
+      if (xDiffAbs === 2 && yDiffAbs === 2) {
+        if (sSum % 2 !== 0) {
+          console.log(`Reason 13`);
+          return { isValid: false };
         }
-
       }
       let pieceToCapture = [x + xDiff / 2, y + yDiff / 2];
 
       //Check if piece to capture is goat
       if (position[pieceToCapture[0]][pieceToCapture[1]] === 1) {
-        console.log(`Reason 8`)
+        console.log(`Reason 8`);
         return {
           isValid: true,
           isCaptureMove: true,
           capturePiece: pieceToCapture,
         };
       } else {
-        console.log(`Reason 9`)
+        console.log(`Reason 9`);
         return { isValid: false };
       }
     }
 
     // Can't move distance more than 2
     if (xDiffAbs > 1 || yDiffAbs > 1) {
-      console.log(`Reason 10`)
+      console.log(`Reason 10`);
       return { isValid: false };
-    }
-
-    // Can't move from odd position to another odd position
+    } // Can't move from odd position to another odd position
     // Example: 0,1 (0+1 = 1 odd) to 1,2 (1+2 = 3 odd)
     else if (sSum % 2) {
       if (tSum % 2) {
-        console.log(`Reason 11`)
+        console.log(`Reason 11`);
         return { isValid: false };
       }
     }
 
-    console.log(`Reason 12`)
+    console.log(`Reason 12`);
     return { isValid: true, isCaptureMove: false };
   };
 
   const value = {
-		turn,
-		setTurn,
-		game,
-		setGame,
-		makeMove,
-		placeGoat,
-		checkMove,
-		goatCounter,
-		goatsCaptured,
-		nextMove,
-		previousMove,
-		moveCounter,
-		moveHistory,
-		gameResult,
-		setGoatCounter,
-		setMoveCounter,
-		setGameResult,
-		setMoveHistory,
-		startingLayout,
-		setGoatsCaptured,
-	};
+    turn,
+    setTurn,
+    game,
+    setGame,
+    makeMove,
+    placeGoat,
+    checkMove,
+    goatCounter,
+    goatsCaptured,
+    nextMove,
+    previousMove,
+    moveCounter,
+    moveHistory,
+    gameResult,
+    setGoatCounter,
+    setMoveCounter,
+    setGameResult,
+    setMoveHistory,
+    startingLayout,
+    setGoatsCaptured,
+    pgn,
+  };
 
   return <GameContext.Provider value={value}>{children}</GameContext.Provider>;
 };
